ba8235b1470d75b2543c0481863fe1e995065df0
r4n_emerson <r4n.oficial@gmail.com>
2023-07-22 17:38:58 -0300

    finalVer

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,13 +1,11 @@
 import os
-
 from flask import Flask
-
 
 def create_app(teste_config = None):
 
     app = Flask(__name__, instance_relative_config = True)
     app.config.from_mapping(
-        SECRET_KEY = 'dev',
+        SECRET_KEY = 'secret-key-aqui',
         DATABASE=os.path.join(app.instance_path, 'flask.sqlite')
     )
 
